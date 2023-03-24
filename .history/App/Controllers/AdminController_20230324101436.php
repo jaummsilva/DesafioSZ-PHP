9cac498d4df59f5f1e8f3d317deaf02e280ea560
João Vitor da Silva <jaumm.silva.04@gmail.com>
2023-03-24 16:30:08 -0300

    Deletar,alterar,cadastrar usuario concluido

---
+++ .history/App/Controllers/AdminController_20230324101436.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AdminController extends Action {
+
+	public function loginAdmin() {
+		$this->renderDeslogado('loginAdmin');
+	}
+    public function homeAdmin() {
+        session_start();
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id',$_SESSION['id']);
+        $this->renderAdmin('homeAdmin');
+    }
+    public function autenticarAdmin() {
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('email',$_POST['email']);
+        $usuario->__set('senha',sha1($_POST['senha']));
+        
+        $usuario->autenticar();
+        if($usuario->__get('id') != '' && $usuario->__get('nome')) {
+            session_start();
+
+            $_SESSION['id'] = $usuario->__get('id');
+            $_SESSION['nome'] = $usuario->__get('nome');
+
+            header('Location: /homeAdmin');
+        }
+        else {
+            header('Location: /login');
+        }
+    }
+    public function cadastroUsuarioAdmin() {
+        session_start();
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id',$_SESSION['id']);
+        $this->renderAdmin('cadastroUsuarioAdmin');
+    }
+    public function registrar() {
+        $this->view->errosUsuario = [];
+        $usuario = Container::getModel('Usuario');
+        $this->view->getUsuariosEmail = $usuario->getTodosUsuariosEmail();
+
+        if(sha1($_POST['senha']) != sha1($_POST['repetirSenha'])) {
+            $this->view->errosUsuario[] = "Senha e Repetir Senha devem ser iguais";
+            $this->renderAdmin('cadastroUsuarioAdmin');
+            return;
+        }
+        if($this->view->getUsuariosEmail == $_POST['email']) {
+            $this->view->errosUsuario[] = "Senha e Repetir Senha devem ser iguais";
+            $this->renderAdmin('cadastroUsuarioAdmin');
+            return;
+        }
+        
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('nome',$_POST['nome']);
+		$usuario->__set('email',$_POST['email']);
+		$usuario->__set('senha',sha1($_POST['senha']));
+        $usuario->__set('data_nascimento',$_POST['data_nascimento']);
+        $usuario->__set('telefone',$_POST['telefone']);
+        $usuario->__set('imagemId',$_POST['imagemId']);
+
+        if($usuario->validarCadastro()) {
+			$usuario->cadastrarUsuario();
+			header('Location: /homeAdmin');
+		}
+		else {
+			header('Location: /cadastroUsuarioAdmin');
+		}
+	}
+    public function listagemUsuarioAdmin() {
+        session_start();
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id',$_SESSION['id']);
+        $this->view->getUsuarios = $usuario->getTodosUsuarios();
+        
+        $this->renderAdmin('listagemUsuarioAdmin');
+    }
+    public function editarUsuarioAdmin($id) {
+        session_start();
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id',$id);
+        $this->view->getUsuario = $usuario->getUsuario();
+        $this->renderAdmin('editarUsuarioAdmin');
+
+
+    }
+    public function editar() {
+        session_start();
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id',$_POST['id']);
+        $usuario->__set('nome',$_POST['nome']);
+		$usuario->__set('email',$_POST['email']);
+		$usuario->__set('senha',sha1($_POST['senha']));
+        $usuario->__set('data_nascimento',$_POST['data_nascimento']);
+        $usuario->__set('telefone',$_POST['telefone']);
+        $usuario->__set('imagemId',$_POST['imagemId']);
+        $usuario->editarUsuario();
+    }
+}
+
+
+?>
---
+++ .history/public/index_20230324112453.php
@@ -0,0 +1,9 @@
+<?php
+
+	//ini_set('error_reporting', 'E_STRICT');
+	require_once "../vendor/autoload.php";
+	date_default_timezone_set('S')
+
+	$route = new \App\Route;
+
+?>
